8f176108ea2121ce79ef74904bc211ed8689fbff
Hope Bradley <hope.e.bradley@gmail.com>
2021-04-06 12:25:44 -0400

    first fetching

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let artworkContainer = document.querySelector('#artwork-container');
 
+    //this function would take data from a fetch and the keyword the user typed in
+   
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=flowers`)
+    .then(resp => resp.json())
+    .then(data => {
+        const objectArray = data.objectIDs;
+        const randomID = Math.floor(Math.random() * 100);
+        console.log(objectArray[randomID]);
+    })
+
 
 
 
